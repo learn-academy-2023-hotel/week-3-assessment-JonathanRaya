b4c7c1a167624c581097a38e61633d02eb2070ac
jonathanraya <jonathanraya79@gmail.com>
2023-10-29 19:30:56 -0500

    Submitting finished assessment for week 3

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -13,7 +13,12 @@
 // --------------------1) Prompt: Create a function that takes in a number (greater than 2) and returns an array containing the Fibonacci sequence. The length of the array is determined by the argument of the function.
 
 // a) Create a test with expect statements for each of the variables provided.
-
+describe ("fibonnacciArray", () => {
+  it("takes in a number (greater than 2) and returns an array containing the Fibonacci sequence. The length of the array is determined by the argument of the function.", () => {
+    expect(fibonnacciArray(fibonacciLength1)).toEqual([1, 1, 2, 3, 5, 8])
+    expect(fibonnacciArray(fibonacciLength2)).toEqual([1, 1, 2, 3, 5, 8, 13, 21, 34, 55])
+  })
+})
 const fibonacciLength1 = 6
 // Expected output: [1, 1, 2, 3, 5, 8]
 
@@ -21,13 +26,44 @@ const fibonacciLength2 = 10
 // Expected output: [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 
 // b) Create the function that makes the test pass.
+// Psuedo code : 
+// Create a function and name it fibonnacciArray
+// Make a new varaible to be able to .push() our result into it 
+// Make a for loop that will iterate as many times as the length of the parameter. Start with i = 2 since newArray will already contain the first two indexes. 
+// get the value at the index -1 by one then add that with the value at the index of the array - 2. Then use .push() to save that value into the next index from newArray.
+// create a conditional statement to verify that the arguement passed is bothe greater than 2 and that it is a number and return the newArray if those conditions are met.
+ // if the conditions of the if statment are not met, then return a string telling the user to input an arguement meeting the conditions.
+const fibonnacciArray = (num) => {
+  let newArray = [1, 1]
 
-// Pseudo code:
+  for ( let i = 2; i < num; i++ ) {
+    newArray.push(newArray[i - 1] + newArray[i - 2])
+  }
+  if (num > 2 && typeof num === 'number')
+  { 
+    return newArray
+  }
+  else 
+  {
+    return "Please input a number or a variable with a value grater than 2."
+  }
+}
+console.log(fibonnacciArray(fibonacciLength1))
+console.log(fibonnacciArray(fibonacciLength2))
+
 
 // --------------------2) For the following prompt, use ChatGPT or an equivalent AI tool to help you solve the problem. The prompt will require the use of a JavaScript method called Object.values()
 // Prompt: Create a function that takes in an object and returns an array of the object's values sorted from least to greatest.
 
 // a) Create a test with expect statements for each of the variables provided.
+
+describe("objInOrder", () => {
+  it("takes in an object and returns an array of the object's values sorted from least to greatest.", () => {
+    expect(objInOrder(studyMinutesWeek1)).toEqual([15, 15, 20, 30, 30, 60, 90])
+    expect(objInOrder(studyMinutesWeek2)).toEqual([10, 15, 20, 45, 60, 65, 100])
+  })
+} )
+
 
 const studyMinutesWeek1 = {
   sunday: 90,
@@ -54,3 +90,22 @@ const studyMinutesWeek2 = {
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// Create a function named objInOrder
+// have the parameter named object to display that an object is the arguement that should be passed
+// assign a variable (minutes) that will be assigned an array of the objects values. Object.values(object): this is used to get values in an object that are numbers and it puts them into an array
+// make an if statement to verify that the arguement being passed is an object. If it is not return a string telling the user to pass an object as the arguement.
+// if it meets the conditions for the if statement, then return the array (minutes) sorted from leat to greatest. use .sort((a, b) => a - b) to do this: this sorts them from least to greatest. We must use a,b => a-b in the paranthesis because it would think 100 is smaller than other numbers like only 25 because it would look at the first number and see that 2 is bigger than 1.
+
+const objInOrder = (object) => {
+  const minutes = Object.values(object)
+  if(typeof object === "object"){
+   return minutes.sort((a, b) => a - b)
+  }
+  else 
+  {
+   return "Please enter an object as the arguement"
+  }
+}
+console.log(objInOrder(studyMinutesWeek1))
+console.log(objInOrder(studyMinutesWeek2))
+
--- interview-questions.md
+++ interview-questions.md
@@ -8,30 +8,38 @@ Then, research the question to expand on your answer. Even if you feel you have 
 
 1. What is JSX? What is one syntax difference between HTML and JSX?
 
-Your answer:
+Your answer: 
+JSX is a combination of JavaScript and HTML used to create React apps. JSX is what is returned in a component in a react application. One syntax difference is that we must use curly braces {} to wrap our JS code in the return. This lets the program know that we are going to be using JS logic or data. We can then wrap all that JS code inside the curly braces with HTML tags. For example we can wrap paragraph tags around our JS code.<p>{JS code here}</p> 
 
 Researched answer:
+JSX stands for JavaScript XML iyt lets us write HTML in React one syntax difference is HTMl only allows static text while JSX allows the input of expressions and functions.
 
 2. What is yarn? What file(s) are modified when you run the command yarn in your terminal?
 
-Your answer:
+Your answer: Yarn is a package manager. It installs files into the direcrtory of your choice that adds node modules and allows you to test your code by writing expect statments.
 
 Researched answer:
+Yarn is a package manager that's primary function is to install dependencies to allow specific snippets of code to be ran. It modifies the package.json and yarn.lock files.
 
 3. What is the difference between state values and props in React?
 
 Your answer:
+State value is the current value of a variable, props are data types that we can pass from a component to the main APP.js file or vice versa. One way to compare state and props is imagining state is water and props is a set temperature. The props can change the state but the prop itself will not change. In our example props (the temperature) can be 50 degrees where it would make state (water) a liquid. However a different prop (10 degrees) will change the state and the water will be frozen. Therefore the state was changed but a prop will not change it will only affect the state.
 
 Researched answer:
+Props pass data from a parent component to a child, state is used to manage data within a component.
 
 4. STRETCH: Which is the difference between a div tag and a span tag?
 
-Researched answer:
+Researched answer: 
+A div tag is used as a container, we can style that container with css by assigning it a class name. A span tag is an inline container that allows us to mark up part of text or a document.
 
 ## Looking Ahead: Terms for Next Week
 
 1. DOM: 
+DOM statnds for Document Object Model, it represents the page so that programs can change the structure, stlye, and content of it.
 
 2. Object-oriented programming:
-
+A computer model that organizes software design around data or objects instead of functions and logic.
 3. Ruby:
+An open source programming language focusing on simplicity and productivity.
